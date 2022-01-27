aa5b7362e24971b141fcc286ef3e18dff785d251
RAHOUANI-Abdessalam <abdelsalamrahouani@gmail.com>
2022-01-27 19:48:08 +0100

    final commit

---
+++ app/src/main/java/com/example/chat/Splashscreen.java
@@ -0,0 +1,33 @@
+package com.example.chat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        },SPLASH_TIMER);
+    }
+}
